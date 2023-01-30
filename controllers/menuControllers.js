a2c81ddf31adc8d4ffa2607c6aa716af3a6fe166
thepjos <josh.thephasdin@gmail.com>
2023-01-30 13:24:34 -0600

    trying to fix delete button still

--- controllers/menuControllers.js
+++ controllers/menuControllers.js
@@ -13,29 +13,29 @@ const router = express.Router()
 
 // ROUTES
 // menu items are a sub document. menu items can only be edited by the owner/logged in user. They can be built by the logged in user. 
-router.put('/:restaurantId', (req, res) => {
+// router.put('/:restaurantId', (req, res) => {
 
-    const menuItems = req.body.dishName
-    console.log('these are the new menu items ', menuItems)
+//     const menuItems = req.body.dishName
+//     console.log('these are the new menu items ', menuItems)
     
-    Restaurant.findById(restaurantId)
+//     Restaurant.findById(restaurantId)
 
-        .then(restaurant => {
-            res.redirect(`/restaurant/${restaurant.id}`)
-        })
-        .catch(err => {
-            console.log(err)
-            res.redirect(`/error?error=${err}`)
-        })
+//         .then(restaurant => {
+//             res.redirect(`/restaurant/${restaurant.id}`)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.redirect(`/error?error=${err}`)
+//         })
 
-})
+// })
 
 
 // DELETE route for ONE MENU ITEM on SHOW PAGE
 
 router.delete('/:restId/:menuId', (req, res) => {
 	//try to isolatte the menus
-    const restId = req.params.restId
+    const restId = req.params.restid
     const menuId = req.params.menuId
     console.log('trying to delete ', restId)
     console.log('trying to delete ', menuId)
@@ -47,17 +47,17 @@ router.delete('/:restId/:menuId', (req, res) => {
             const theMenuItem = restaurant.menuItems.id(menuId)
             console.log('this is the menu item i want to delete ', theMenuItem)
 			if (restaurant.owner == req.session.userId) {
-                // console.log('trying to delete ', restaurantId)
-                // console.log('trying to delete ', menuId)
-				//return restaurant.deleteOne()
+                theMenuItem.remove()
+                restaurant.save()
+                res.redirect(`/restaurant/mine`)
 			} else {
 				res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20delete%20this%20menu%20item`)
 			}
 		})
-		.then(() => {
+		// .then(() => {
 
-			res.redirect(`/restaurant/mine`)
-		})
+		// 	res.redirect(`/restaurant/mine`)
+		// })
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
 		})
